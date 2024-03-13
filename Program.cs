98d9aeb69232047774be348579f0bdcbf376c6ed
ZussiMane <igoryunja@gmail.com>
2024-03-13 21:00:18 +0300

    Добавил условие 2-ой задачи

--- Program.cs
+++ Program.cs
@@ -6,22 +6,35 @@
 // a=7 => Нет
 // a=322 => Да
 
-Console.WriteLine("Введите чиcло: ");
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите чиcло: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите первое число для проверки на кратность: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число для проверки на кратность: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число для проверки на кратность: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-int multiplicitytonum1 = a % num1;
-int multiplicitytonum2 = a % num2;
+// Console.WriteLine("Введите второе число для проверки на кратность: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int multiplicitytonum1 = a % num1;
+// int multiplicitytonum2 = a % num2;
 
-if (multiplicitytonum1 == 0 && multiplicitytonum2 == 0)
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Нет");
-}
+// if (multiplicitytonum1 == 0 && multiplicitytonum2 == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+// --------------------------------------------------------------//
+// Задача 2: Напишите программу, которая принимает 
+// на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 
+// 0 и выдаёт номер координатной четверти плоскости, 
+// в которой находится эта точка.
+// Пример: 
+// 2, 3 => 1
+// -5, 3 => 2
+// -3, -2 => 3
+// 4, -2 => 4
+
+
+
