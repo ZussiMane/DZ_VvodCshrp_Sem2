47be1c368392870aa7d917859f8be51bdff1d655
ZussiMane <igoryunja@gmail.com>
2024-03-13 20:21:59 +0300

    Добавил решение 1-ой задачи

--- Program.cs
+++ Program.cs
@@ -2,6 +2,26 @@
 // на вход число и проверяет, кратно ли оно 
 // одновременно 7 и 23
 // Пример:
-// a=50 => нет
-// a=7 => нет
-// a=322 => да
+// a=50 => Нет
+// a=7 => Нет
+// a=322 => Да
+
+Console.WriteLine("Введите чиcло: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое число для проверки на кратность: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число для проверки на кратность: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int multiplicitytonum1 = a % num1;
+int multiplicitytonum2 = a % num2;
+
+if (multiplicitytonum1 == 0 && multiplicitytonum2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
