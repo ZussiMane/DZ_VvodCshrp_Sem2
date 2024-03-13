f01d300eb48990ff5bbb264b8126a25fd17dba37
ZussiMane <igoryunja@gmail.com>
2024-03-13 21:05:38 +0300

    Добавил решение 2-ой задачи

--- Program.cs
+++ Program.cs
@@ -36,5 +36,33 @@
 // -3, -2 => 3
 // 4, -2 => 4
 
+Console.WriteLine("Введите первую координату точки: ");
+int x = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите вторую координату точки: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("2");
+    }
+
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("3");
+    }
+
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("4");
+    }
+
+    else
+    {
+        Console.WriteLine("Ошибка ввода! Смотри условие!");
+    }
 
